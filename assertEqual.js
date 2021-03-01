4b7130a37c73074980071d1dfde5191bc3718bd7
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-01 23:31:59 +0000

    created  console.assert and played around with emojies

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🤩🤩🤩 Assertion Passed: " + actual + " === " + expected);
+  } else
+    console.log("🤢🤢🤢 Assertion Failed: " + actual + " !== " + expected);
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(4, 82);
+assertEqual("LukeBergmann", "LukeBergmann");
