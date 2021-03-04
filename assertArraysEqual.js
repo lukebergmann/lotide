c1d41fbe6ba0d7de8c57741c7beed4a5fb4cd270
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-04 02:31:39 +0000

    Fixed the bug that wasn't allowing the right answer

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,34 @@
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  let firstArray = [];
-  let secondArray = [];
-  for (let i = 0; i < arrayOne.length; i++) {
-    firstArray = arrayOne[i];
-  }
-  for (let j = 0; j < arrayTwo.length; j++) {
-    secondArray = arrayTwo[j];
-  }
-  if (firstArray === secondArray) {
-    return true;
-  } else {
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
     return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayTwo[i] !== arrayOne[i]) {
+        return false;
+      }
+    }
   }
+  return true;
 };
 
-const assertEqual = function(actual, expected) {
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🤩🤩🤩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+
+const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`🤩🤩🤩 Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual([1, 2, 3], [1, 2, 3]);
+
+(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+(assertArraysEqual([1, 6, 3], [1, 2, 3]));
+(assertArraysEqual([1, 2, "Banana"], [1, 2, 3]));
