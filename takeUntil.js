37fceb1d99833fa507133048f53592e989938a0d
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-05 04:37:42 +0000

    created a function and callback that stops and returns a loop when a certain condition is met

---
+++ takeUntil.js
@@ -0,0 +1,55 @@
+const takeUntil = function(array, callback) {
+  let arr = [];
+  for (let element of array) {
+    if(!callback(element)){
+      arr.push(element);
+    } else {
+      return arr;
+    }
+  }
+};
+
+const eqArrays = function (arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayTwo[i] !== arrayOne[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🤩🤩🤩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🤩🤩🤩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+ 
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
