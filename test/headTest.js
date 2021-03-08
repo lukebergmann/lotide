a8d22b841f49023975ce43dd01e7cfb18b677c4c
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-08 22:19:16 +0000

    implemented mocha and chai to run my test codes rather than manual testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,16 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Turkey", "Lighthouse", "Labs"]), "Hello");
+describe('grabs the "head", or index[0] of an array', () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
+
+// * Manual Test Code 
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Turkey", "Lighthouse", "Labs"]), "Hello");
