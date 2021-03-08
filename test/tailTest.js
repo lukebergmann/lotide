005bafc2971b1ee4e50fcb16654230948bd8e7ec
Luke Bergmann <lukebergmann0@gmail.com>
2021-03-08 23:03:51 +0000

    modified tailTest.js using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual'); 
 
-assertArraysEqual(tail(["1", "2", "3"]), ["2", "3"]);
-assertArraysEqual(tail(["1", "5", "3"]), ["2", "3"]);
-assertArraysEqual(tail(["1", "7", "3"]), ["2", "3"]);
+describe('grabs the "tail, or every index OTHER than index[0] of an array', () => {
+  it("returns 2 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
+
+
+
+// * Manual Test Code * //
+// assertArraysEqual(tail(["1", "2", "3"]), ["2", "3"]);
+// assertArraysEqual(tail(["1", "5", "3"]), ["2", "3"]);
+// assertArraysEqual(tail(["1", "7", "3"]), ["2", "3"]);
